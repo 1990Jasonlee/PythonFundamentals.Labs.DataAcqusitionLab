2aa399321461f6428a383954743d18af03ca6f98
Jason <leejason1990@gmail.com>
2022-04-17 12:08:22 -0400

    update

--- make_requests.py
+++ make_requests.py
@@ -1,6 +1,9 @@
 import urllib.request
-
+import os
 file = 0
 
 while file < 39:
     url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/'
+    header = {'token': 'hPxdeiarThkOjtyXSiykGemaretddPq'}
+    req = urllib.request.Request(url, headers=header)
+    file_name = '.locations'+str(file)+'.json'
